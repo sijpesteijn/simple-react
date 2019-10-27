7761f42ada4582e20561649b89a67cad275d16ba
gijs.sijpesteijn <gijs@jpoint.nl>
2019-10-27 18:23:56 +0100

    Simple react rest

--- src/main/java/nl/sijpesteijn/simplereact/HelloController.java
+++ src/main/java/nl/sijpesteijn/simplereact/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/react/hello")
 public class HelloController {
 
     @GetMapping
