a99e324bc83f661449701b1c27f18efe4b488119
gijs.sijpesteijn <gijs@jpoint.nl>
2019-10-27 18:21:33 +0100

    Simple react rest

--- src/main/java/nl/sijpesteijn/simplereact/HelloController.java
+++ src/main/java/nl/sijpesteijn/simplereact/HelloController.java
@@ -1,4 +1,4 @@
-package nl.sijpesteijn.simplerest;
+package nl.sijpesteijn.simplereact;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
